9950fcea315570d479c1b542754c8c920ddc9946
Luke Harold Miles <luke@isi.edu>
2021-05-20 14:46:31 -0400

    convert /redirect from request library to axios library

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const express = require('express');
 const app = express();
 const session = require('express-session');
 const request = require('request');
+const axios = require('axios');
 const qs = require('querystring');
 const randomString = require('randomstring');
 
@@ -40,23 +41,18 @@ app.all('/redirect', (req, res) => {
   const code = req.query.code;
   const returnedState = req.query.state;
   if (req.session.csrf_string === returnedState) {
-    request.post(
-      {
-        url:
-          'https://github.com/login/oauth/access_token?' +
-          qs.stringify({
-            client_id: process.env.CLIENT_ID,
-            client_secret: process.env.CLIENT_SECRET,
-            code: code,
-            redirect_uri: redirect_uri,
-            state: req.session.csrf_string
-          })
-      },
-      (error, response, body) => {
-        req.session.access_token = qs.parse(body).access_token;
+    axios.post('https://github.com/login/oauth/access_token?' +
+      qs.stringify({
+        client_id: process.env.CLIENT_ID,
+        client_secret: process.env.CLIENT_SECRET,
+        code: code,
+        redirect_uri: redirect_uri,
+        state: req.session.csrf_string
+      }), {})
+      .then(response => {
+        req.session.access_token = qs.parse(response.data).access_token;
         res.redirect('/user');
-      }
-    );
+      });
   } else {
     res.redirect('/');
   }
@@ -74,8 +70,8 @@ app.get('/user', (req, res) => {
     (error, response, body) => {
       res.send(
         "<p>You're logged in! Here's all your emails on GitHub: </p>" +
-          body +
-          '<p>Go back to <a href="./">log in page</a>.</p>'
+        body +
+        '<p>Go back to <a href="./">log in page</a>.</p>'
       );
     }
   );
